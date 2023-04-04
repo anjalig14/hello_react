134e3ca0ae9c025f5467b3e95838f14bb193d3fd
anjalig14 <gullapa8@msu.edu>
2023-04-03 23:44:04 -0400

    Code

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function Library() {
         <tr>
           <td>{b.title}</td>
           <td>{b.author}</td>
-          <td>{b.ISBN}</td>
+          <td>{b.isbn}</td>
         </tr>
       ))
     }
